c964b74719a3b2c419558798963340d2c1d956fe
Strecon <strecon@yandex.ru>
2021-04-09 13:19:06 +0300

    Add news link in menu by route

--- resources/views/nav/siteMenu.blade.php
+++ resources/views/nav/siteMenu.blade.php
@@ -3,7 +3,7 @@
             <ul class="nav navbar-nav">
                 <li class="navbar-li active"><a href="/">Home</a></li>
                 <li class="navbar-li"><a href="/about">About</a></li>
-                <li class="navbar-li"><a href="/news">News</a></li>
+                <li class="navbar-li"><a href="{{ route('news::categories') }}">News</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="navbar-li"><a href="#">Sign in</a></li>
