66dd99d8a6d4c4ea48c6bc41a04a1547c7554b2d
Strecon <strecon@yandex.ru>
2021-04-09 12:47:41 +0300

    Add simle links in menu

--- resources/views/nav/siteMenu.blade.php
+++ resources/views/nav/siteMenu.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
             <ul class="nav navbar-nav">
-                <li class="navbar-li active"><a href="#">Home</a></li>
-                <li class="navbar-li"><a href="#">About</a></li>
-                <li class="navbar-li"><a href="#">News</a></li>
+                <li class="navbar-li active"><a href="/">Home</a></li>
+                <li class="navbar-li"><a href="/about">About</a></li>
+                <li class="navbar-li"><a href="/news">News</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li class="navbar-li"><a href="#">Sign in</a></li>
